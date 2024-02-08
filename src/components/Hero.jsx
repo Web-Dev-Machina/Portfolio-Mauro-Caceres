0fa475c5381176646647f7c3b04fb708063beb7f
Mauro Caceres <caceres_mauro22789@outlook.com>
2024-02-08 19:41:16 -0300

    Responsive Home

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,9 +2,11 @@ import { TypeAnimation } from 'react-type-animation';
 
 const Hero = () => {
     return <>
+    {/* responsive img */}
+    <img src="user.png" alt="billing" className="top-5 absolute w-[400px] h-[650px] md:w-0 md:h-0 -z-5 opacity-10" />
+
       <section className="flex md:flex-row flex-col items-center justify-between">
-        <div className="xl:px-0 sm:px-16 px-6">
-  
+        <div className="py-44 xl:px-0 sm:px-16 px-6">
           <div className="text-left">
             <h1 className="font-poppins font-semibold text-[50px] sm:text-[60px] md:text-[52px] lg:text-[70px] xl:text-[75px] text-white leading-[75px] lg:leading-[100px]">
               ¡Hola! Soy
@@ -29,17 +31,18 @@ const Hero = () => {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt labore quidem cum ullam unde nobis laborum tempora officia sint at perspiciatis, debitis perferendis ad repellat, veniam laudantium expedita minima iure?
           </p>
         </div>
+        {/* responsive gradients */}
+        <div className="rounded-full bg-red-400 absolute w-96 h-96 md:hidden blur-[300px] top-0 right-0 opacity-60"/>
+        <div className="rounded-full bg-purple-800 absolute w-96 h-96 md:hidden blur-[300px] top-0 right-0 opacity-60"/>
         </div>
   
         <div className="flex relative">
-          <img src="user.png" alt="billing" className="w-[500px] relative z-[5]" />
-  
-          {/* gradient start */}
-          <div className="rounded-full bg-red-400 absolute w-96 h-96 blur-[300px] right-0"/>
-          <div className="rounded-full bg-purple-600 absolute w-96 h-96 blur-[300px] bottom-0 left-0"/>
-          {/* gradient end */}
-        </div>
 
+        <img src="user.png" alt="billing" className="hidden md:inline-block w-[500px] z-[5]" />
+          {/* gradients*/}
+          <div className="hidden md:inline-block rounded-full bg-red-400 absolute w-96 h-96 blur-[300px] right-0"/>
+          <div className="hidden md:inline-block rounded-full bg-purple-600 absolute w-96 h-96 blur-[300px] bottom-0 left-0"/>
+        </div>
       </section>
       </>;
   };
