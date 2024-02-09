d5242c8a543e3e7e4a691f5d5bd9609a89fe8450
Mauro Caceres <caceres_mauro22789@outlook.com>
2024-02-09 01:03:45 -0300

    Add Toggle and Hero fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import Navbar from "./components/Navbar.jsx"
 import Hero from "./components/Hero.jsx"
 import StarCanvas from "./canvas/StarCanvas.jsx"
+import Switch from "./components/Switch.jsx"
+
 
 function App() {
 
-  return (
-    <>
+  return <>
       <div className="relative z-0">
+        <Switch/>
         <StarCanvas/>
         <Navbar/>
         <Hero/>
       </div>
+
     </>
-  )
 }
 
 export default App
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,10 +3,11 @@ import { TypeAnimation } from 'react-type-animation';
 const Hero = () => {
     return <>
     {/* responsive img */}
-    <img src="user.webp" alt="billing" className="top-5 absolute w-[400px] h-[650px] md:w-0 md:h-0 -z-5 opacity-10" />
+    <img src="user.webp" alt="user" className="top-5 absolute w-[400px] h-[650px] md:w-0 md:h-0 -z-5 opacity-10" />
 
       <section className="flex md:flex-row flex-col items-center justify-between">
-        <div className="py-44 xl:px-0 sm:px-16 px-6">
+
+        <div className="py-32 xl:px-0 sm:px-16 px-6">
           <div className="text-left">
             <h1 className="font-poppins font-semibold text-[50px] sm:text-[60px] md:text-[52px] lg:text-[70px] xl:text-[75px] text-white leading-[75px] lg:leading-[100px]">
               ¡Hola! Soy
@@ -30,7 +31,13 @@ const Hero = () => {
           <p className="max-w-[470px] md:max-w-[550px] mt-5 text-white">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt labore quidem cum ullam unde nobis laborum tempora officia sint at perspiciatis, debitis perferendis ad repellat, veniam laudantium expedita minima iure?
           </p>
+          <div className='text-red-500 space-x-5 space-y-10'>
+            <button><i className="fa-brands fa-github bg-purple-700/10 backdrop-blur-sm text-3xl rounded-full p-4 px-5"></i></button>
+            <button><i className="fa-brands fa-linkedin bg-purple-700/10 backdrop-blur-sm text-3xl rounded-full p-4 px-5"></i></button>
+            <button><i className="fa-brands fa-whatsapp bg-purple-700/10 backdrop-blur-sm text-3xl rounded-full p-4 px-5"></i></button>
+          </div>
         </div>
+
         {/* responsive gradients */}
         <div className="rounded-full bg-red-400 absolute w-96 h-96 md:hidden blur-[300px] top-0 right-0 opacity-60"/>
         <div className="rounded-full bg-purple-800 absolute w-96 h-96 md:hidden blur-[300px] top-0 right-0 opacity-60"/>
@@ -38,7 +45,7 @@ const Hero = () => {
   
         <div className="flex relative">
 
-        <img src="user.webp" alt="billing" className="hidden md:inline-block w-[500px] z-[5]" />
+        <img src="user.webp" alt="user" className="hidden md:inline-block w-[500px] z-[5]" />
           {/* gradients*/}
           <div className="hidden md:inline-block rounded-full bg-red-400 absolute w-96 h-96 blur-[300px] right-0"/>
           <div className="hidden md:inline-block rounded-full bg-purple-600 absolute w-96 h-96 blur-[300px] bottom-0 left-0"/>
