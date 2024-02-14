29e387e63b7c1545cbf41c100ea13ffb7cea7a1b
Web Ex Machina <main.webexmachina@gmail.com>
2024-02-13 23:54:37 -0300

    Spline Computer

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,21 @@ import Hero from "./components/Hero.jsx"
 import StarCanvas from "./canvas/StarCanvas.jsx"
 import Switch from "./components/Switch.jsx"
 import Work from "./components/Work.jsx"
+import Profile from "./components/Profile.jsx"
 
 function App() {
 
   return <>
   <Switch/>
-      <div className="relative z-0">        
-        <StarCanvas/>
-        <Hero/>
-      </div>
-      <Work/>
-      <Navbar/>
+    {/*StarCanvas box*/}
+    <div className="relative z-0">        
+      <StarCanvas/>
+      <Hero/>
+    </div>
+    <Work/>
+    <Profile/>
+
+    <Navbar/>
     </>
 }
 
---
+++ src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import ComputerSpline from "./ComputerSpline";
+
+const Profile = () => {
+  return <>
+  
+  <section className="px-4">
+    <div className="flex px-4 max-w-screen-xl mx-auto">
+        <h2 className="py-16 font-poppins font-semibold text-[40px] sm:text-[60px] md:text-[44px] lg:text-[60px] xl:text-[65px] text-white leading-[75px] lg:leading-[100px]">
+          Mi <span className="text-red-500 red-glow">Experiencia</span>
+        </h2>
+    </div>
+
+    <ComputerSpline/>
+
+  </section>
+  </>;
+};
+
+export default Profile;
