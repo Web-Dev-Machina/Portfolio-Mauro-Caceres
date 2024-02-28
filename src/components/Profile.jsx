b02fb8dd7a6663acc8f8336f48e87a18837aa13b
Web Ex Machina <main.webexmachina@gmail.com>
2024-02-28 20:47:51 -0300

    Experience grid background update

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -15,11 +15,11 @@ const Profile = () => {
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt labore quidem cum ullam unde nobis laborum tempora officia sint at perspiciatis, debitis perferendis ad repellat, veniam laudantium expedita minima iure?
       </p>
 
-      <div className="grid grid-cols-2 gap-5 justify-evenly">  
-        <div className="bg-green-700 w-26 h-12">1</div>  
-        <div className="bg-green-500 w-26 h-12">2</div>  
-        <div className="bg-green-300 w-26 h-12">3</div>  
-        <div className="bg-green-700 w-26 h-12">4</div>  
+      <div className="grid grid-cols-2 gap-10 py-10">  
+        <div className="bg-purple-950/30 text-white backdrop-blur-sm rounded-full">1</div>  
+        <div className="bg-purple-950/30 text-white backdrop-blur-sm rounded-full">2</div>  
+        <div className="bg-purple-950/30 text-white backdrop-blur-sm rounded-full">3</div>  
+        <div className="bg-purple-950/30 text-white backdrop-blur-sm rounded-full">4</div>  
       </div> 
 
 
