458a0f73495e8d4a852b18dd77846fc5187c470b
Web Ex Machina <main.webexmachina@gmail.com>
2024-02-28 23:12:34 -0300

    Experience complete

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -15,14 +15,159 @@ const Profile = () => {
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt labore quidem cum ullam unde nobis laborum tempora officia sint at perspiciatis, debitis perferendis ad repellat, veniam laudantium expedita minima iure?
       </p>
 
-      <div className="grid grid-cols-2 gap-10 py-10">  
-        <div className="bg-purple-950/30 text-white backdrop-blur-sm rounded-full">1</div>  
-        <div className="bg-purple-950/30 text-white backdrop-blur-sm rounded-full">2</div>  
-        <div className="bg-purple-950/30 text-white backdrop-blur-sm rounded-full">3</div>  
-        <div className="bg-purple-950/30 text-white backdrop-blur-sm rounded-full">4</div>  
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 py-10 max-w-screen-md mx-auto">
+
+        <div className="bg-purple-950/45 text-white backdrop-blur-sm rounded-2xl">
+
+          <h3 className="py-2 font-poppins font-semibold text-[30px] sm:text-[45px] md:text-[35px] lg:text-[45px] xl:text-[50px] text-red-500"><span className="red-glow">Front-End</span></h3>
+
+          <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-3 mx-auto gap-5 py-4">
+
+            <div className="mx-auto">
+              <img className="mx-auto" src="https://skillicons.dev/icons?i=html" alt="HTML" />
+              <p className="mx-auto">HTML</p>
+            </div>
+
+            <div className="mx-auto">
+              <img className="mx-auto" src="https://skillicons.dev/icons?i=css" alt="css" />
+              <p className="mx-auto">CSS</p>
+            </div>
+
+            <div className="mx-auto">
+              <img className="mx-auto" src="https://skillicons.dev/icons?i=javascript" alt="javascript" />
+              <p className="mx-auto">Javascript</p>
+            </div>
+
+            <div className="mx-auto">
+              <img className="mx-auto" src="https://skillicons.dev/icons?i=react" alt="react" />
+              <p className="mx-auto">React</p>
+            </div>
+
+            <div className="mx-auto">
+              <img className="mx-auto" src="https://skillicons.dev/icons?i=tailwind" alt="tailwind" />
+              <p className="mx-auto">Tailwind</p>
+            </div>
+
+            <div className="mx-auto">
+              <img className="mx-auto" src="https://skillicons.dev/icons?i=sass" alt="sass" />
+              <p className="mx-auto">SASS</p>
+            </div>
+
+            <div className="mx-auto">
+              <img className="mx-auto" src="https://skillicons.dev/icons?i=vite" alt="vite" />
+              <p className="mx-auto">Vite</p>
+            </div>
+
+            <div className="mx-auto">
+              <img className="mx-auto" src="https://skillicons.dev/icons?i=threejs" alt="threejs" />
+              <p className="mx-auto">Threejs</p>
+            </div>
+
+            <div className="mx-auto">
+              <img className="mx-auto" src="https://skillicons.dev/icons?i=bootstrap" alt="bootstrap" />
+              <p className="mx-auto">Bootstrap</p>
+            </div>
+
+          </div>
+        </div>  
+
+        <div className="bg-purple-950/45 text-white backdrop-blur-sm rounded-2xl">
+
+          <h3 className="py-2 font-poppins font-semibold text-[30px] sm:text-[45px] md:text-[35px] lg:text-[45px] xl:text-[50px] text-red-500"><span className="red-glow">Back-End</span></h3>
+
+          <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-3 mx-auto gap-5 py-4">
+
+            <div className="mx-auto">
+              <img className="mx-auto" src="https://skillicons.dev/icons?i=express" alt="express" />
+              <p className="mx-auto">Express</p>
+            </div>
+
+            <div className="mx-auto">
+              <img className="mx-auto" src="https://skillicons.dev/icons?i=nodejs" alt="nodejs" />
+              <p className="mx-auto">NodeJS</p>
+            </div>
+
+            <div className="mx-auto">
+              <img className="mx-auto" src="https://skillicons.dev/icons?i=firebase" alt="firebase" />
+              <p className="mx-auto">Firebase</p>
+            </div>
+
+            <div className="mx-auto">
+              <img className="mx-auto" src="https://skillicons.dev/icons?i=mongodb" alt="mongodb" />
+              <p className="mx-auto">MongoDB</p>
+            </div>
+
+            <div className="mx-auto">
+              <img className="mx-auto" src="https://skillicons.dev/icons?i=mysql" alt="mysql" />
+              <p className="mx-auto">MySQL</p>
+            </div>
+
+            </div>
+          </div>
+
+        <div className="bg-purple-950/45 text-white backdrop-blur-sm rounded-2xl">
+
+          <h3 className="py-2 font-poppins font-semibold text-[30px] sm:text-[45px] md:text-[35px] lg:text-[45px] xl:text-[50px] text-red-500"><span className="red-glow">Herramientas</span></h3>
+
+          <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-3 mx-auto gap-5 py-4">
+
+            <div className="mx-auto">
+              <img className="mx-auto" src="https://skillicons.dev/icons?i=git" alt="git" />
+              <p className="mx-auto">Git</p>
+            </div>
+
+            <div className="mx-auto">
+              <img className="mx-auto" src="https://skillicons.dev/icons?i=npm" alt="npm" />
+              <p className="mx-auto">npm</p>
+            </div>
+
+            <div className="mx-auto">
+              <img className="mx-auto" src="https://skillicons.dev/icons?i=netlify" alt="netlify" />
+              <p className="mx-auto">Netlify</p>
+            </div>
+
+            <div className="mx-auto">
+              <img className="mx-auto" src="https://skillicons.dev/icons?i=vercel" alt="vercel" />
+              <p className="mx-auto">Vercel</p>
+            </div>
+
+            <div className="mx-auto">
+              <img className="mx-auto" src="https://skillicons.dev/icons?i=github" alt="github" />
+              <p className="mx-auto">GitHub</p>
+            </div>
+
+            <div className="mx-auto">
+              <img className="mx-auto" src="https://skillicons.dev/icons?i=vscode" alt="vscode" />
+              <p className="mx-auto">VSCode</p>
+            </div>
+
+            <div className="mx-auto">
+              <img className="mx-auto" src="https://skillicons.dev/icons?i=sublime" alt="sublime" />
+              <p className="mx-auto">Sublime</p>
+            </div>
+
+          </div>
+        </div> 
+
+        <div className="bg-purple-950/45 text-white backdrop-blur-sm rounded-2xl">
+
+          <h3 className="py-2 font-poppins font-semibold text-[30px] sm:text-[45px] md:text-[35px] lg:text-[45px] xl:text-[50px] text-red-500"><span className="red-glow">Aprendiendo</span></h3>
+
+          <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-3 mx-auto gap-5 py-4">
+
+            <div className="mx-auto">
+              <img className="mx-auto" src="https://skillicons.dev/icons?i=typescript" alt="typescript" />
+              <p className="mx-auto">Typescript</p>
+            </div>
+
+            <div className="mx-auto">
+              <img className="mx-auto" src="https://skillicons.dev/icons?i=nextjs" alt="nextjs" />
+              <p className="mx-auto">NextJS</p>
+            </div>
+
+          </div>
+        </div> 
       </div> 
-
-
     </div>
     
     {/*
