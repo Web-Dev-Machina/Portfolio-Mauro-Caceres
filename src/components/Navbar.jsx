df39241582a07a0b2eb6e8032a26aa2cf4c7dede
Web Ex Machina <main.webexmachina@gmail.com>
2024-03-01 16:54:40 -0300

    meta open graph html

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-
 const Navbar = () => {
   return <>
     <nav className="flex flex-col items-center xl:justify-center gap-y-4 fixed h-max bottom-0 mt-auto xl:right-[2%] z-[1000] top-0 w-full xl:w-16 xl:max-w-md xl:h-screen px-5 sm:px-10 xl:px-0" >
