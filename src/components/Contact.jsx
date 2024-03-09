2f057dcbfb8fb47078e52602c67519e50b415a91
Web Ex Machina <main.webexmachina@gmail.com>
2024-03-08 21:48:54 -0300

    Contact button color update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,17 +4,19 @@ const Contact = () => {
   
     <section className="px-4">
 
-        <div className="flex max-w-screen-xl mx-auto">
-            <h2 className="mx-auto md:mx-0 py-28 font-poppins font-semibold text-[30px] sm:text-[40px] md:text-[33px] lg:text-[40px] xl:text-[50px] text-white leading-[75px] lg:leading-[100px]">
+        <div className="py-28 flex max-w-screen-xl mx-auto">
+            <h2 className="mx-auto md:mx-0 font-poppins font-semibold text-[30px] sm:text-[40px] md:text-[33px] lg:text-[40px] xl:text-[50px] text-white leading-[75px] lg:leading-[100px]">
                 Mi <span className="text-red-500 red-glow">Contacto</span>
                 </h2>
+
+                <p></p>
         </div>
 
         <div className=" max-w-screen-xl mx-auto">
 
             <div className="grid grid-cols-1 gap-5 max-w-screen-md mx-auto">
 
-                <div className="bg-purple-950/30 text-white backdrop-blur-2xl rounded-full">
+                <div className="bg-purple-950/10 text-white backdrop-blur-2xl rounded-full">
                     <div className="py-4">Nombre</div>
                 </div>
 
@@ -26,7 +28,7 @@ const Contact = () => {
                     <div className="py-4">Escriba su consulta</div>
                 </div> 
 
-                <button href="#" className="neon-button max-w-44 mx-auto">Enviar</button>
+                <button href="#" className="neon-button max-w-44 mx-auto backdrop-blur-sm">Enviar</button>
             </div> 
         </div>
 
