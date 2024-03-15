c191beab32c4a88df9b4d8c87c864492fad76176
Web Ex Machina <main.webexmachina@gmail.com>
2024-03-15 17:15:16 -0300

    Nav observer fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,19 +10,19 @@ const Navbar = (props) => {
       <div className="shadow-2xl text-red-500 flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-10 md:px-40 xl:px-0 h-[55px] xl:h-max xl:py-8 bg-purple-700/10 backdrop-blur-sm text-3xl rounded-full">
 
         <button aria-label="Go to Hero" onClick={() => clickHandler("hero")}>
-          <i className={`fa-sharp fa-light fa-rocket-launch text-red-500 transition linear duration-500 ${inView[0] ? "text-red-200" : ""}`}></i>
+          <i className={`fa-sharp fa-light fa-rocket-launch text-red-500 transition linear duration-500 ${inView[0] ? "text-red-100" : ""}`}></i>
           </button>
 
         <button aria-label="Go to Projects " onClick={() => clickHandler("projects")}>
-          <i className={`fa-light fa-laptop-code text-red-500 transition linear duration-500 ${inView[1] ? "text-red-200" : ""}`}></i>
+          <i className={`fa-light fa-laptop-code text-red-500 transition linear duration-500 ${inView[1] ? "text-red-100" : ""}`}></i>
         </button>
 
         <button aria-label="Go to Experience" onClick={() => clickHandler("experience")}>
-          <i className={`fa-light fa-user text-red-500 transition linear duration-500 ${inView[2] ? "text-red-200" : ""}`}></i>
+          <i className={`fa-light fa-user text-red-500 transition linear duration-500 ${inView[2] ? "text-red-100" : ""}`}></i>
         </button>
 
         <button aria-label="Go to Contact" onClick={() => clickHandler("contact")}>
-          <i className={`fa-light fa-comments text-red-500 transition linear duration-500 ${inView[3] ? "text-red-200" : ""}`}></i>
+          <i className={`fa-light fa-comments text-red-500 transition linear duration-500 ${inView[3] ? "text-red-100" : ""}`}></i>
         </button>
 
       </div>
