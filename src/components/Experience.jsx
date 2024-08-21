afe1217fa5f68f0f2fb14a6d96dd0883572210a3
Web-Dev-Machina <webdevmachina@gmail.com>
2024-08-21 13:50:41 -0300

    Update and Skill changes 3

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -159,8 +159,7 @@ const Experience = () => {
             {/* <div className="mx-auto">
               <img className="mx-auto" src="https://skillicons.dev/icons?i=nextjs" alt="nextjs" />
               <p className="mx-auto">NextJS</p>
-            </div> 
-            
+            </div>
             */}
 
           </div>
