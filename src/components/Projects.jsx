c159c73d21af39287a0f17948ab1f78192436615
Mauro Ezequiel Cáceres <116774245+Web-Dev-Machina@users.noreply.github.com>
2025-04-08 21:54:07 -0300

    Update Projects.jsx Landing Page link

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -57,7 +57,7 @@ const Projects = () => {
           <div loading="lazy" className="img-resp-child flex flex-row items-center justify-center gap-12 h-auto w-[350px] md:w-[400px] lg:[500px] bg-cover text-center bg-center overflow-hidden" style={{backgroundImage: "url('card3.webp')"}}>
             <div className="button-child gap-12 hidden">
               <a aria-label="Go to Github repository" target="_blank" rel="noopener noreferrer" href="https://github.com/FGA-Consultora/FGA"><div className="fa-brands fa-github text-red-500 hover:text-white bg-purple-950/70 backdrop-blur-sm text-3xl rounded-full p-4 px-5"></div></a>
-              <a aria-label="Go to Website" target="_blank" rel="noopener noreferrer" href="https://www.fgaconsultora.com.ar/"><div className="fa-solid fa-play text-red-500 hover:text-white bg-purple-950/70 backdrop-blur-sm text-3xl rounded-full p-4 px-5"></div></a>
+              <a aria-label="Go to Website" target="_blank" rel="noopener noreferrer" href="https://fgaconsultora.netlify.app/"><div className="fa-solid fa-play text-red-500 hover:text-white bg-purple-950/70 backdrop-blur-sm text-3xl rounded-full p-4 px-5"></div></a>
             </div>
           </div>
           <div className="flex flex-col bg-purple-700/10 backdrop-blur-sm p-5 justify-between leading-normal py-5 w-auto">
